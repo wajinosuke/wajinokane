58314865e413c296a150fc0aebda616a56a5ddc2
wajinosuke <w2physalis+github@gmail.com>
2018-09-17 06:25:27 +0900

    csv読み込み完成

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ const main = async () => {
     const converter = new CSVConverter('./test_files/jaccs.json');
     try {
         const data = await converter.convertCsv('./test_files/WebDetail201809170253.csv');
-        console.log(data);
+       // console.log(data);
     } catch (e) {
-        console.log('エラー');
+        console.log(e);
     }
 }
 
